4ab13cc200482d46bbb3eea39515d1076e07ddda
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-10-05 09:40:35 -0700

    Update TwoClickCircle.java

--- TwoClickCircle.java
+++ TwoClickCircle.java
@@ -9,17 +9,17 @@ import java.awt.geom.Point2D;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
-public class TwoClickCircleComponent extends JPanel
+public class TwoClickCircle extends JPanel
 {
-	private static final long serialVersionUID = 1L;
-	public TwoClickCircleComponent()
+	
+	public TwoClickCircle()
 	{
 		
 		track = 0;
 		center = null;
 		onCircle = null;
 		
-		class MouseSpy extends MouseAdapter
+		class MousePressListener extends MouseAdapter
 		{
 			
 			
@@ -42,18 +42,20 @@ public class TwoClickCircleComponent extends JPanel
 				}
 				
 				track++;
+				System.out.println(event.getX() + " " + event.getY());
+				
 				repaint();
 				
 			}
 			
 		}
 		
-		MouseSpy listener = new MouseSpy();
+		MouseListener listener = new MousePressListener();
 		addMouseListener(listener);
 		
 	}
 	
-	public void paintCompoent(Graphics g)
+	public void paintComponent(Graphics g)
 	{
 		
 		super.paintComponent(g);
@@ -62,19 +64,35 @@ public class TwoClickCircleComponent extends JPanel
 		if(center != null)
 		{
 			plotPoint(g2, center);
+			System.out.println(center);
 			
-			if(onCircle != null)
-			{
-				plotPoint(g2, onCircle);
+			
+			//if(onCircle != null)
+			//{
+				//plotPoint(g2, onCircle);
 				/*
 				double diameter = 2 * (Math.sqrt( Math.pow(onCircle.getX()-center.getX(), 2) + 
 						Math.pow(onCircle.getY() - center.getY(), 2)) );
 				g2.drawOval((int)(center.getX() - diameter/2),(int)( center.getX() - diameter/2),(int) diameter,(int) diameter);
 				*/
-				drawCircle(g2);
+				//drawCircle(g2);
 				
 				
-			}
+			//}
+			
+			
+		}
+		
+		if(onCircle != null)
+		{
+			//plotPoint(g2, onCircle);
+			/*
+			double diameter = 2 * (Math.sqrt( Math.pow(onCircle.getX()-center.getX(), 2) + 
+					Math.pow(onCircle.getY() - center.getY(), 2)) );
+			g2.drawOval((int)(center.getX() - diameter/2),(int)( center.getX() - diameter/2),(int) diameter,(int) diameter);
+			*/
+			drawCircle(g2, center, onCircle);
+			
 			
 		}
 		
@@ -91,20 +109,28 @@ public class TwoClickCircleComponent extends JPanel
 		double x = point.getX();
 		double y = point.getY();
 		
-		Ellipse2D.Double dot = new Ellipse2D.Double(x - 5, y - 5, 10, 10);
+		Ellipse2D.Double dot = new Ellipse2D.Double(x - 5, y -5 , 10, 10);
+		g2.draw(dot);
 		g2.fill(dot);
-		
 	}
 	
-	public void drawCircle(Graphics2D g2)
+	public void drawCircle(Graphics2D g2, Point2D.Double c, Point2D.Double onCirc)
 	{
 		
-		double diameter = 2 * (Math.sqrt( Math.pow(onCircle.getX()-center.getX(), 2) + 
-				Math.pow(onCircle.getY() - center.getY(), 2)) );
+		Double onCircX = onCirc.getX();
+		Double onCircY = onCirc.getY();
 		
-		Ellipse2D.Double circle = new Ellipse2D.Double((int)(center.getX() - diameter/2), (int)( center.getX() - diameter/2),
-				(int) diameter, (int) diameter);
+		Double cX = c.getX();
+		Double cY = c.getY();
+		
+		double diameter = 2 * (Math.sqrt( Math.pow(onCircX-cX, 2) + Math.pow(onCircY - cY, 2)) );
+		
+		Ellipse2D.Double circle = new Ellipse2D.Double((cX - diameter/2), (cY - diameter/2),
+				diameter, diameter);
 		g2.draw(circle);
+		System.out.println(circle);
+		
+		plotPoint(g2, onCirc);
 		
 	}
 	
